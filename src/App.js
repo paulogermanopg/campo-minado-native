13391dad2428e1f41a1e34b351992c39d4ae550b
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-01 14:43:13 -0300

    campos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import params from './params'
+import Field from './components/Field'
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.container}>Iniciando as Minas</Text>
         <Text style={styles.container}>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+        <Field />
       </View>
     )
   }
