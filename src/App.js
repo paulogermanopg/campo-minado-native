aec0392c431d6f9550441f00b30b0b4b57496184
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-03-01 14:16:31 -0300

    criando parâmetros

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import params from './params'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.container}>Iniciando as Minas</Text>
+        <Text style={styles.container}>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+      </View>
+    )
+  }
+  
 }
 
 const styles = StyleSheet.create({
